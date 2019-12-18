8ebe396ae8090d9f91894c2032981e6d59067089
hirauchg <gentahirauchi@gmail.com>
2019-12-19 07:42:20 +0900

    アプリ情報画面の実装

--- app/src/main/java/com/hirauchi/fortune/MainActivity.kt
+++ app/src/main/java/com/hirauchi/fortune/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.hirauchi.fortune
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.hirauchi.appinfolibrary.AppInfoActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.tool_bar))
+        supportActionBar?.setTitle(R.string.app_name)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_app_info -> startActivity(Intent(this, AppInfoActivity::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
