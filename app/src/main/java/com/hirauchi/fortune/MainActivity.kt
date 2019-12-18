edf74ffb2c3e9037e35fa21a62f7fb9cd6d6a866
hirauchg <gentahirauchi@gmail.com>
2019-12-19 07:47:49 +0900

    adMobの実装

--- app/src/main/java/com/hirauchi/fortune/MainActivity.kt
+++ app/src/main/java/com/hirauchi/fortune/MainActivity.kt
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 import com.hirauchi.appinfolibrary.AppInfoActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +20,11 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(findViewById(R.id.tool_bar))
         supportActionBar?.setTitle(R.string.app_name)
+
+        MobileAds.initialize(this) {}
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
